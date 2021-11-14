219867f07cf205cb60bcdc52693130d167f38d85
Oleg Tokarev <77736385+zxc3buttons@users.noreply.github.com>
2021-11-14 21:51:19 +0300

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -59,14 +59,17 @@ bool Graph::isConnectedGraph(int st) {
     for (int i = 0; i < numVertices; i++)
         if (visited[i]) amountOfReachedVertices++;
 
-    if (amountOfReachedVertices == numVertices)
+    if (amountOfReachedVertices == numVertices) {
+        cout << "Graph is connected" << endl;
         return true;
-
+    }
+    
+    cout << "Graph is disconnected";
     return false;
 
 }
 
-void Graph::Dijkstra(int st, int end) {
+int* Graph::Dijkstra(int st, int end) {
 
     int* distance = new int[numVertices];
     int* arrayPath = new int[numVertices];
@@ -106,6 +109,16 @@ void Graph::Dijkstra(int st, int end) {
     cout << distance[end] << endl;
     cout << endl;
 
+    return distance;
+}
+
+void Graph::outputShortestPath(int st, int end, int* distance) {
+
+    int* arrayPath = new int[numVertices];
+
+    for (int i = 0; i < numVertices; i++)
+        arrayPath[i] = 0;
+
     arrayPath[end] = 1;
 
     while (end != st) {
@@ -122,7 +135,7 @@ void Graph::Dijkstra(int st, int end) {
     cout << "Path from start to end vertex:" << endl;
     cout << endl;
     for (int i = 0; i < numVertices; i++)
-        if (arrayPath[i] != 0) cout << i << " " << endl;
+        if (arrayPath[i] != 0) cout << i << " ";
 }
 
 Graph::~Graph() {
